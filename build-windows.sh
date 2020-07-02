c4ab1021654615db2cd37e26aa7e5c595349cb33
Haroldo <haroldo.santos@gmail.com>
2020-07-01 21:07:17 -0300

    windows

--- build-windows.sh
+++ build-windows.sh
@@ -1,7 +1,21 @@
 #! /bin/sh
 
-echo "test"
+echo ""
+echo "Starting Windows build with MSYS2"
+echo ""
 
-ls -lhct
+echo "Installing dependencies"
+
+pacman -Syu  --noconfirm
+pacman -S base-devel gcc  --noconfirm
+pacman -S mingw-w64-x86_64-lapack mingw-w64-x86_64-winpthreads-git mingw-w64-x86_64-readline \
+    mingw-w64-x86_64-suitesparse mingw-w64-x86_64-metis --noconfirm
+
+gcc --version
+g++ --version
+gfortran --version
+
+wget https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew
+chmod u+x ./coinbrew
  
 
